ec7386deaeb6658e5e235822f02dde9d84efb61f
MoyuScript <moyuscript@gmail.com>
2023-02-01 17:50:44 +0800

    Init

---
+++ electron.vite.config.js
@@ -0,0 +1,26 @@
+import { resolve } from 'path'
+import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from 'tailwindcss'
+
+export default defineConfig({
+  main: {
+    plugins: [externalizeDepsPlugin()]
+  },
+  preload: {
+    plugins: [externalizeDepsPlugin()]
+  },
+  renderer: {
+    resolve: {
+      alias: {
+        '@renderer': resolve('src/renderer/src')
+      }
+    },
+    plugins: [react()],
+    css: {
+      postcss: {
+        plugins: [tailwindcss]
+      }
+    }
+  }
+})
